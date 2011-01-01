7ca9616a78f1d47c41106a30b47398d3c031ea8f
Simon Tucker <s.tucker@dcs.shef.ac.uk>
2011-01-01 15:16:28 +0000

    First Pass

---
+++ src/main/java/uk/co/brotherlogic/mdb/Want.java
@@ -0,0 +1,47 @@
+package uk.co.brotherlogic.mdb;
+
+public class Want implements Comparable<Want> {
+
+	@Override
+	public int compareTo(Want arg0) {
+		return (author + title).hashCode()
+				- (arg0.author + arg0.title).hashCode();
+	}
+
+	public int getRecordID() {
+		return recordID;
+	}
+
+	@Override
+	public String toString() {
+		if (recordID >= 0)
+			return author + " - " + title + " [" + recordID + "]";
+		else
+			return author + " - " + title;
+	}
+
+	String author;
+	String title;
+	int recordID;
+
+	public Want(String line) {
+		this("", "");
+		String[] elems = line.trim().split("~");
+		if (elems.length >= 2) {
+			author = elems[0];
+			title = elems[1];
+		}
+		if (elems.length == 3)
+			recordID = Integer.parseInt(elems[2]);
+	}
+
+	public Want(String aut, String tit) {
+		this(aut, tit, -1);
+	}
+
+	public Want(String aut, String tit, int id) {
+		author = aut;
+		title = tit;
+		recordID = -1;
+	}
+}
---
+++ src/main/java/uk/co/brotherlogic/mdb/WantList.java
@@ -0,0 +1,74 @@
+package uk.co.brotherlogic.mdb;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+import uk.co.brotherlogic.mdb.record.GetRecords;
+import uk.co.brotherlogic.mdb.record.Record;
+
+public class WantList implements Comparable<WantList> {
+	String name;
+	List<Want> wants = new LinkedList<Want>();
+
+	@Override
+	public int compareTo(WantList o) {
+		return -scoreList().compareTo(o.scoreList());
+	}
+
+	public Want pickWant() {
+		Collections.sort(wants);
+		for (Want want : wants) {
+			if (want.getRecordID() < 0)
+				return want;
+		}
+		return null;
+	}
+
+	private Double scoreList() {
+		double total = 0.0;
+		double count = 0.0;
+		for (Want want : wants) {
+			if (want.getRecordID() >= 0) {
+				try {
+					Record r = GetRecords.create()
+							.getRecord(want.getRecordID());
+					if (r == null)
+						System.err.println(want);
+					total += r.getScore(User.getUser("Simon"));
+					count += 1;
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		if (count > 0)
+			return total / count;
+		else
+			return 0.0;
+	}
+
+	public static WantList loadFromFile(File f) {
+		WantList list = new WantList();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(f));
+
+			list.name = reader.readLine().trim();
+			for (String line = reader.readLine(); line != null; line = reader
+					.readLine()) {
+				list.wants.add(new Want(line));
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+}
